12e6ced62d94ee5f46ff06ccdec97f1490628554
Ragir Fernando Aparecido da Silva <ragirfernando@gmail.com>
2020-09-21 15:01:52 -0300

    first commit

---
+++ src/main/java/cursosonline/CursosOnlineApplication.java
@@ -0,0 +1,13 @@
+package cursosonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursosOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cursosonline/CursosOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package cursosonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
