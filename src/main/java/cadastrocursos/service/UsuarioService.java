29885b1deed8e34a3bfd8a1ee84f2683b117294c
Ragir Fernando Aparecido da Silva <ragirfernando@gmail.com>
2020-09-29 12:47:55 -0300

    Adicionar a lista de perfis ao GrantedAuthority.

--- src/main/java/cadastrocursos/service/UsuarioService.java
+++ src/main/java/cadastrocursos/service/UsuarioService.java
@@ -6,7 +6,6 @@ import cadastrocursos.exceptions.ResourceNotFoundException;
 import cadastrocursos.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -25,28 +24,28 @@ public class UsuarioService implements UserDetailsService {
     public List<Usuario> listarUsuarios() {
         return usuarioRepository.findAll();
     }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Usuario usuario = Optional.ofNullable(usuarioRepository.findByUsername(username)).orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado"));
-        String[] perfis = listarPerfisUsuario(usuario.getPerfis());
-
+        List<String> perfis = addListaPerfis(usuario.getPerfis());
         List<GrantedAuthority> roles = createAuthorityList(perfis);
-
-        /*List<GrantedAuthority> authorityListAdmin = AuthorityUtils.createAuthorityList();
-        List<GrantedAuthority> authorityListUser = AuthorityUtils.createAuthorityList(user);*/
         return new User(usuario.getUsername(), usuario.getPassword(), roles);
     }
 
-    private String[] listarPerfisUsuario(Set<Perfil> perfis) {
-        String[] roles = (String[]) perfis.toArray();
-        return roles;
+    private List<String> addListaPerfis(Set<Perfil> perfis) {
+        List<String> listaPerfis = new ArrayList<>();
+        perfis.forEach(perfil -> {
+            listaPerfis.add(perfil.getNome());
+        });
+        return listaPerfis;
     }
 
-    public static List<GrantedAuthority> createAuthorityList(String... roles) {
-        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>(roles.length);
-        for (String role : roles) {
+    public static List<GrantedAuthority> createAuthorityList(List<String> roles) {
+        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>(roles.size());
+        roles.forEach(role -> {
             authorities.add(new SimpleGrantedAuthority(role));
-        }
+        });
         return authorities;
     }
 
